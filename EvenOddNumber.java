f21df47da5cbc73c4f75e34a14a60ea03e4e7fd0
IT25103576 <it25103576@my.sliit.lk>
2026-02-08 22:44:46 +0530

    Q4A

---
+++ EvenOddNumber.java
@@ -0,0 +1,11 @@
+public class EvenOddNumber {
+
+    // Method to check whether a number is even or odd
+    public boolean findEvenOrOdd(int i) {
+        if (i % 2 == 0) {
+            return true;   // true means even
+        } else {
+            return false;  // false means odd
+        }
+    }
+}
